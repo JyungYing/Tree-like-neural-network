35a422f64adb4c32bbf1aa8b78f0a0163567bf98
LiChunYing <39322845+JyungYing@users.noreply.github.com>
2019-05-19 22:08:30 +0800

    Update DNN-cla-cv-Diabetes.py

--- classification/original code/DNN-cla-cv-Diabetes.py
+++ classification/original code/DNN-cla-cv-Diabetes.py
@@ -1,5 +1,5 @@
 #=======================================
-## DNN_cla_Diabetes (Cross validation)
+## DNN - Diabetes
 #=======================================
 
 # Need compile Create_DNN_model.
@@ -31,41 +31,39 @@ from keras.layers import Dropout
 import matplotlib.pyplot as plt
 
 #----------------------------------------
-# Load the Diabetes Compressive Strength dataset.
+# Load the Diabetes dataset.
 
 dataset = np.loadtxt("Diabetes-Data.txt")
 
-type(dataset)
+print(type(dataset))
 
-dataset.shape
-
-which = lambda status: np.arange(len(status))[status]
+print(dataset.shape)
 
 index = [1, 4, 5]
 
 X = dataset[:, index]
 Y = dataset[:, 8]
 
-np.unique(Y)
+print(np.unique(Y))
 
-X.shape
-Y.shape
+print(X.shape)
+print(Y.shape)
 
 #----------------------------------------
 # Standardize the input and output data.
 
 X_sample_mean = np.mean(X, axis = 0)
-np.round(X_sample_mean, 4)
+print(np.round(X_sample_mean, 4))
 
 X_sample_std = np.std(X, axis = 0, ddof = 1)
-np.round(X_sample_std, 4)
+print(np.round(X_sample_std, 4))
 
 standardize = lambda x: (x - np.mean(x, axis = 0)) / np.std(x, axis = 0, ddof = 1)
 
 X_train = standardize(X)
 
-np.round(np.mean(X_train, axis = 0), 4)
-np.round(np.std(X_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(X_train, axis = 0), 4))
+print(np.round(np.std(X_train, axis = 0, ddof = 1), 4))
 
 Y_train = Y
 
@@ -122,7 +120,6 @@ cv_acc = []
 for k in range(fold_num):
     # Build the model.
     model = create_DNN_model(nodes, drop, kernel, act)
-    #model.summary()
     # Compile the model.
     model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
     # Select index sets.
@@ -130,38 +127,37 @@ for k in range(fold_num):
     test_index = index[seq_index]
     train_index = np.delete(index, seq_index)
     # Fit the model.
-    history = model.fit(X_train[train_index], Y_train[train_index], validation_split = 0.2, epochs = 1000, batch_size = 40, verbose = 2)
+    history = model.fit(X_train[train_index], Y_train[train_index], validation_split = 0.2, epochs = 1000, batch_size = 40, verbose = 1)
     # Evaluate the model.
-    scores = model.evaluate(X_train[test_index], Y_train[test_index], verbose = 0)
+    scores = model.evaluate(X_train[test_index], Y_train[test_index], verbose = 1)
     print("%s: %.4f" % (model.metrics_names[0], scores[0]))
     print("%s: %.4f" % (model.metrics_names[1], scores[1]))
     cv_bc.append(scores[0])
     cv_acc.append(scores[1])
 
+model.summary()	
+	
 print("%.4f (+/- %.4f)" % (np.mean(cv_bc), np.std(cv_bc)))
 print("%.4f (+/- %.4f)" % (np.median(cv_bc), np.std(cv_bc)))
 
 print("%.4f (+/- %.4f)" % (np.mean(cv_acc), np.std(cv_acc)))
 print("%.4f (+/- %.4f)" % (np.median(cv_acc), np.std(cv_acc)))
 
-result = {'loss_metric': scores}
-
 #----------------------------------------
-# plot the model.
+# Predict
 
-from keras.utils import plot_model
+model.predict(X_train, verbose = 1)
 
-plot_model(model, to_file = 'DNN_diabetes_model0.png')
-plot_model(model, show_shapes = True, to_file = 'DNN_diabetes_model1.png')
+result = {'history': history.history, 'loss_metric': scores}
 
 #----------------------------------------
 # Save the result to NPZ file
 
-np.savez('D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\DNN_cla_Diabetes_cv_result', **result)
+np.savez('C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Diabetes\\DNN_cv_result(Diabetes)', **result)
 
 # Load the result of NPZ file
 
-result = np.load('D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\DNN_cla_Diabetes_cv_result.npz')
+result = np.load('C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Diabetes\\DNN_cv_result(Diabetes).npz')
 
 result.files
 
@@ -169,14 +165,23 @@ result['history']
 result['loss_metric']
 
 #----------------------------------------
+# plot the model.
+
+from keras.utils import plot_model
+
+plot_model(model, to_file = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Figures\\Tree-like\\Classification\\Diabetes\\DNN_diabetes_model0.png')
+plot_model(model, show_shapes = True, to_file = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Figures\\Tree-like\\Classification\\Diabetes\\DNN_diabetes_model1.png')
+
+
+#----------------------------------------
 # Save the model to H5 file
 
-file_path_hdf5 = 'D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\DNN_cla_Diabetes_cv.h5'
+file_path_hdf5 = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Diabetes\\DNN_cv(Diabetes).h5'
 model.save(file_path_hdf5)
 
 # Load model of H5 file
 
-file_path_hdf5 = 'D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\DNN_cla_Diabetes_cv.h5'
+file_path_hdf5 = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Diabetes\\DNN_cv(Diabetes).h5'
 loaded_model = load_model(file_path_hdf5)
 
 #----------------------------------------
@@ -187,27 +192,25 @@ history.history.keys()
 #----------------------------------------
 # Summarize history for loss.
 
-plt.ion()
-
-plt.figure('Figure Diabetes loss', figsize = (4.8, 4.0))
+plt.figure('DNN Diabetes loss', figsize = (4.8, 4.0))
 plt.plot(history.history['loss'], "r-")
 plt.plot(history.history['val_loss'], "b--")
-plt.title('Training/validating loss')
+plt.title('DNN Diabetes' + '\n' + 'Training/validating loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['training loss', 'validating loss'], loc = "best", frameon = False)
-#plt.show()
+plt.show()
 
 #----------------------------------------
 # Summarize history for metric.
 
-plt.figure('Figure Diabetes metric', figsize = (4.8, 4.0))
+plt.figure('DNN Diabetes metric', figsize = (4.8, 4.0))
 plt.plot(history.history['acc'], "r-")
 plt.plot(history.history['val_acc'], "b--")
-plt.title('Training/validating metric')
+plt.title('DNN Diabetes' + '\n' + 'Training/validating metric')
 plt.ylabel('acc')
 plt.xlabel('epoch')
 plt.legend(['training metric', 'validating metric'], loc = "best", frameon = False)
-#plt.show()
+plt.show()
 
 ###################
