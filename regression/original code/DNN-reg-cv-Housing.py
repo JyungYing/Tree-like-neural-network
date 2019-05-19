1940b7ffcc1d8502932e9ff87e58a6563c1876e9
LiChunYing <39322845+JyungYing@users.noreply.github.com>
2019-05-19 22:21:30 +0800

    Update DNN-reg-cv-Housing.py

--- regression/original code/DNN-reg-cv-Housing.py
+++ regression/original code/DNN-reg-cv-Housing.py
@@ -1,5 +1,5 @@
 #=======================================
-## DNN_reg_Housing (Cross_validation)
+## DNN - Housing
 #=======================================
 
 # Need compile Create_DNN_model.
@@ -36,7 +36,7 @@ import matplotlib.pyplot as plt
 
 dataset = np.loadtxt("Housing-Training-Data.txt")
 
-dataset.shape
+print(dataset.shape)
 
 index = [0, 5, 7, 12]
 
@@ -47,27 +47,27 @@ Y = dataset[:, 13]
 # Standardize the input and output data.
 
 X_sample_mean = np.mean(X, axis = 0)
-np.round(X_sample_mean, 4)
+print(np.round(X_sample_mean, 4))
 
 X_sample_std = np.std(X, axis = 0, ddof = 1)
-np.round(X_sample_std, 4)
+print(np.round(X_sample_std, 4))
 
 Y_sample_mean = np.mean(Y, axis = 0)
-np.round(Y_sample_mean, 4)
+print(np.round(Y_sample_mean, 4))
 
 Y_sample_std = np.std(Y, axis = 0, ddof = 1)
-np.round(Y_sample_std, 4)
+print(np.round(Y_sample_std, 4))
 
 standardize = lambda x:(x - np.mean(x, axis = 0))/np.std(x, axis = 0, ddof = 1)
 
 X_train = standardize(X)
 Y_train = standardize(Y)
 
-np.round(np.mean(X_train, axis = 0), 4)
-np.round(np.std(X_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(X_train, axis = 0), 4))
+print(np.round(np.std(X_train, axis = 0, ddof = 1), 4))
 
-np.round(np.mean(Y_train, axis = 0), 4)
-np.round(np.std(Y_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(Y_train, axis = 0), 4))
+print(np.round(np.std(Y_train, axis = 0, ddof = 1), 4))
 
 #----------------------------------------
 # Define 10-fold cross validation test index sets.
@@ -129,13 +129,15 @@ for k in range(fold_num):
     test_index = index[seq_index]
     train_index = np.delete(index, seq_index)
     # Fit the model.
-    history = model.fit(X_train[train_index], Y_train[train_index], epochs = 1000, validation_split = 0.2, batch_size = 20, verbose = 2)
+    history = model.fit(X_train[train_index], Y_train[train_index], epochs = 1000, validation_split = 0.2, batch_size = 20, verbose = 1)
     # Evaluate the model.
-    scores = model.evaluate(X_train[test_index], Y_train[test_index], verbose = 0)
+    scores = model.evaluate(X_train[test_index], Y_train[test_index], verbose = 1)
     print("%s: %.4f" % (model.metrics_names[0], scores[0]))
     print("%s: %.4f" % (model.metrics_names[1], scores[1]))
     cv_mse.append(scores[0])
     cv_mae.append(scores[1])
+
+model.summary()
 
 print("%.4f (+/- %.4f)" % (np.mean(cv_mse), np.std(cv_mse)))
 print("%.4f (+/- %.4f)" % (np.median(cv_mse), np.std(cv_mse)))
@@ -143,19 +145,25 @@ print("%.4f (+/- %.4f)" % (np.median(cv_mse), np.std(cv_mse)))
 print("%.4f (+/- %.4f)" % (np.mean(cv_mae), np.std(cv_mae)))
 print("%.4f (+/- %.4f)" % (np.median(cv_mae), np.std(cv_mae)))
 
-result = {'loss_metric': loss_metric}
+#----------------------------------------
+# Predict
+
+model.predict(X_train, verbose = 1)
+
+result = {'history': history.history, 'loss_metric': scores}
 
 #----------------------------------------
 # Save the result to NPZ file
 
-np.savez('D:\\Li-Jyung-Ying\\Keras-Objects\\tree-like-nn\\Regression\\DNN_Housing_cv_result', **result)
+np.savez('C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Regression\\Housing\\DNN_cv_result(Housing)', **result)
 
 # Load the result of NPZ file
 
-result = np.load('D:\\Li-Jyung-Ying\\Keras-Objects\\tree-like-nn\\Regression\\DNN_Housing_cv_result.npz')
+result = np.load('C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Regression\\Housing\\DNN_cv_result(Housing).npz')
 
 result.files
 
+result['history']
 result['loss_metric']
 
 #----------------------------------------
@@ -163,18 +171,18 @@ result['loss_metric']
 
 from keras.utils import plot_model
 
-plot_model(model, to_file = 'DNN_housing_model0.png')
-plot_model(model, show_shapes = True, to_file = 'DNN_housing_model1.png')
+plot_model(model, to_file = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Figures\\Tree-like\\Regression\\Housing\\DNN_housing_model0.png')
+plot_model(model, show_shapes = True, to_file = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Figures\\Tree-like\\Regression\\Housing\\DNN_housing_model1.png')
 
 #----------------------------------------
 # Save the model to HDF5 file
 
-file_path_hdf5 = 'D:\\Li-Jyung-Ying\\Keras-Objects\\tree-like-nn\\Regression\\DNN_Housing_cv'
+file_path_hdf5 = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Regression\\Housing\\DNN_cv(Housing)'
 model.save(file_path_hdf5)
 
 # Load model of HDF5 file
 
-file_path_hdf5 = 'D:\\Li-Jyung-Ying\\Keras-Objects\\tree-like-nn\\Regression\\DNN_Housing_cv'
+file_path_hdf5 = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Regression\\Housing\\DNN_cv(Housing)'
 laoded_model = load_model(file_path_hdf5)
 
 #----------------------------------------
@@ -185,16 +193,14 @@ history.history.keys()
 #----------------------------------------
 # Summarize history for loss.
 
-plt.ion()
-
 plt.figure('DNN Housing loss', figsize = (4.8, 4.0))
 plt.plot(history.history['loss'], "r-")
 plt.plot(history.history['val_loss'], "b--")
-plt.title('DNN Housing Training/validating loss')
+plt.title('DNN Housing' + '\n' + 'Training/validating loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['training loss', 'validating loss'], loc = "best", frameon = False)
-#plt.show()
+plt.show()
 
 #----------------------------------------
 # Summarize history for metric.
@@ -202,11 +208,11 @@ plt.legend(['training loss', 'validating loss'], loc = "best", frameon = False)
 plt.figure('DNN Housing metric', figsize = (4.8, 4.0))
 plt.plot(history.history['mean_absolute_error'], "r-")
 plt.plot(history.history['val_mean_absolute_error'], "b--")
-plt.title('DNN Housing Training/validating metric')
+plt.title('DNN Housing' + '\n' + 'Training/validating metric')
 plt.ylabel('mae')
 plt.xlabel('epoch')
 plt.legend(['training metric', 'validating metric'], loc = "best", frameon = False)
-#plt.show()
+plt.show()
 
 #----------------------------------------
 # Make prediction.
@@ -214,7 +220,7 @@ plt.legend(['training metric', 'validating metric'], loc = "best", frameon = Fal
 fitted = model.predict(X_train)
 fitted = fitted[:, 0]
 
-fitted.shape
+print(fitted.shape)
 
 # Plot
 
@@ -222,11 +228,10 @@ all = np.concatenate((Y_train, fitted))
 draw_min = np.floor(np.min(all))
 draw_max = np.ceil(np.max(all))
 
-plt.figure('DNN_Housing')
-#plt.plot(Y_train, fitted, 'wo', markersize = 2, markeredgecolor = "black")
+plt.figure('DNN_Housing', figsize = (4.8, 4.0))
 plt.plot(fitted, Y_train, 'wo', markersize = 2, markeredgecolor = "black")
 plt.plot([draw_min, draw_max], [draw_min, draw_max], '-', linewidth = 1, color = 'red')
-plt.title('DNN Housing Observed versus fitted values')
+plt.title('DNN Housing' + '\n' + 'Observed versus fitted values')
 plt.ylabel('observed values')
 plt.xlabel('fitted values')
-#plt.show()
+plt.show()
