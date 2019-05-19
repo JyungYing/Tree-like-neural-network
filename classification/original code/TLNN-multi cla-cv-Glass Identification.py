69c40f0ca6b6cd7953405fd0086cfa33f5df163f
LiChunYing <39322845+JyungYing@users.noreply.github.com>
2019-05-19 22:12:20 +0800

    Update TLNN-multi cla-cv-Glass Identification.py

--- classification/original code/TLNN-multi cla-cv-Glass Identification.py
+++ classification/original code/TLNN-multi cla-cv-Glass Identification.py
@@ -1,15 +1,15 @@
 #=======================================
-## Tree-like neural network - Glass-Identification-Data
+## Tree-like neural network - Glass-Identification
 #=======================================
 
 ###################
 
-# Decision tree: R codes: Glass-Identification-Data Compressive Strength dataset.
+# Decision tree: R codes: Glass-Identification dataset.
 
 #----------------------------------------
-# Load the Glass-Identification-Data dataset.
+# Load the Glass-Identification dataset.
 
-data_original = read.table("D:/Li-Chun-Ying/Data-Sets/classification/Glass-Identification-Data.txt", header = FALSE, sep = ',')
+data_original = read.table("C:/Users/jghsieh/Desktop/Li-Chun-Ying/Data-Sets/classification/Glass-Identification-Data.txt", header = FALSE, sep = ',')
 
 data_original[,11] = factor(data_original[,11])
 
@@ -46,7 +46,7 @@ glass_ctree
 
 ###################
 
-# Neural Network：Python codes：Glass-Identification-Data
+# Neural Network：Python codes：Glass-Identification
 
 #----------------------------------------
 # Change the current working directory to the specified path.
@@ -71,37 +71,39 @@ set_random_seed(seed)
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Model, load_model
-from keras.layers import Input, Dense, Activation, concatenate, Dropout
-#from keras.layers import add, subtract, multiply, average, maximum, Lambda, dot
+from keras.layers import Input, Dense, concatenate, Dropout
 
 #----------------------------------------
-# Load the Glass-Identification-Data Compressive Strength dataset.
+# Load the Glass-Identification dataset.
 
 dataset = np.loadtxt("Glass-Identification-Data.txt", delimiter=",")
 
-type(dataset)
+print(type(dataset))
 
-dataset.shape
+print(dataset.shape)
 
 x_group = [[8], [4], [7], [3]]
 
 X = dataset[:, 0:10]
 Y = dataset[:, 10]
 
-X.shape
-Y.shape
+print(X.shape)
+print(Y.shape)
 
 X1 = dataset[:, x_group[0]]
 X2 = dataset[:, x_group[1]]
 X3 = dataset[:, x_group[2]]
 X4 = dataset[:, x_group[3]]
 
-X1.shape; X2.shape; X3.shape; X4.shape
-Y.shape
+print(X1.shape)
+print(X2.shape)
+print(X3.shape)
+print(X4.shape)
+print(Y.shape)
 
 Y = np.array(Y, dtype = int)
 
-np.unique(Y)
+print(np.unique(Y))
 
 #----------------------------------------
 # one-hot encoding of output variable.
@@ -111,40 +113,40 @@ Y_train = np.zeros((len(Y), 7), dtype = int)
 for i in range(len(Y)):
 	Y_train[i, Y[i] - 1] = 1
 
-np.unique(Y_train)
+print(np.unique(Y_train))
 
 #----------------------------------------
 # Standardize the input data.
 
 X_sample_mean = np.mean(X, axis = 0)
-np.round(X_sample_mean, 4)
+print(np.round(X_sample_mean, 4))
 
 X_sample_std = np.std(X, axis = 0, ddof = 1)
-np.round(X_sample_std, 4)
+print(np.round(X_sample_std, 4))
 
 X1_sample_mean = X_sample_mean[x_group[0]]
-np.round(X1_sample_mean, 4)
+print(np.round(X1_sample_mean, 4))
 
 X1_sample_std = X_sample_std[x_group[0]]
-np.round(X1_sample_std, 4)
+print(np.round(X1_sample_std, 4))
 
 X2_sample_mean = X_sample_mean[x_group[1]]
-np.round(X2_sample_mean, 4)
+print(np.round(X2_sample_mean, 4))
 
 X2_sample_std = X_sample_std[x_group[1]]
-np.round(X2_sample_std, 4)
+print(np.round(X2_sample_std, 4))
 
 X3_sample_mean = X_sample_mean[x_group[2]]
-np.round(X3_sample_mean, 4)
+print(np.round(X3_sample_mean, 4))
 
 X3_sample_std = X_sample_std[x_group[2]]
-np.round(X3_sample_std, 4)
+print(np.round(X3_sample_std, 4))
 
 X4_sample_mean = X_sample_mean[x_group[3]]
-np.round(X4_sample_mean, 4)
+print(np.round(X4_sample_mean, 4))
 
 X4_sample_std = X_sample_std[x_group[3]]
-np.round(X4_sample_std, 4)
+print(np.round(X4_sample_std, 4))
 
 standardize = lambda x: (x - np.mean(x, axis = 0)) / np.std(x, axis = 0, ddof = 1)
 
@@ -155,17 +157,17 @@ X2_train = X_train[:, x_group[1]]
 X3_train = X_train[:, x_group[2]]
 X4_train = X_train[:, x_group[3]]
 
-np.round(np.mean(X1_train, axis = 0), 4)
-np.round(np.std(X1_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(X1_train, axis = 0), 4))
+print(np.round(np.std(X1_train, axis = 0, ddof = 1), 4))
 
-np.round(np.mean(X2_train, axis = 0), 4)
-np.round(np.std(X2_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(X2_train, axis = 0), 4))
+print(np.round(np.std(X2_train, axis = 0, ddof = 1), 4))
 
-np.round(np.mean(X3_train, axis = 0), 4)
-np.round(np.std(X3_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(X3_train, axis = 0), 4))
+print(np.round(np.std(X3_train, axis = 0, ddof = 1), 4))
 
-np.round(np.mean(X4_train, axis = 0), 4)
-np.round(np.std(X4_train, axis = 0, ddof = 1), 4)
+print(np.round(np.mean(X4_train, axis = 0), 4))
+print(np.round(np.std(X4_train, axis = 0, ddof = 1), 4))
 
 #----------------------------------------
 # Define 10-fold cross validation test index sets.
@@ -235,8 +237,6 @@ for k in range(fold_num):
     response = y_final
     #
     model = Model(inputs = [input_g1, input_g2, input_g3, input_g6], outputs = response)
-    #
-    #model.summary()
     # Compile the model.
     model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
     # Select index sets.
@@ -245,31 +245,38 @@ for k in range(fold_num):
     train_index = np.delete(index, seq_index)
     # Fit the model.
     history = model.fit([X1_train[train_index], X2_train[train_index], X3_train[train_index], X4_train[train_index]]
-                        , Y_train[train_index], validation_split = 0.2, epochs = 1000, batch_size = 20, verbose = 2)
+                        , Y_train[train_index], validation_split = 0.2, epochs = 1000, batch_size = 20, verbose = 1)
     # Evaluate the model.
     scores = model.evaluate([X1_train[test_index], X2_train[test_index], X3_train[test_index], X4_train[test_index]]
-                            , Y_train[test_index], verbose = 0)
+                            , Y_train[test_index], verbose = 1)
     print("%s: %.4f" % (model.metrics_names[0], scores[0]))
     print("%s: %.4f" % (model.metrics_names[1], scores[1]))
     cv_cc.append(scores[0])
     cv_acc.append(scores[1])
 
+model.summary()
+	
 print("%.4f (+/- %.4f)" % (np.mean(cv_cc), np.std(cv_cc)))
 print("%.4f (+/- %.4f)" % (np.median(cv_cc), np.std(cv_cc)))
 
 print("%.4f (+/- %.4f)" % (np.mean(cv_acc), np.std(cv_acc)))
 print("%.4f (+/- %.4f)" % (np.median(cv_acc), np.std(cv_acc)))
 
+#----------------------------------------
+# Predict
+
+model.predict([X1_train, X2_train, X3_train, X4_train], verbose = 1)
+
 result = {'history': history.history, 'loss_metric': scores}
 
 #----------------------------------------
 # Save the result to NPZ file
 
-np.savez('D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Tree_like_nn_cla_cv_result(Glass-Identification)', **result)
+np.savez('C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Glass-Identification\\Tree_like_nn_cv_result(Glass-Identification)', **result)
 
 # Load the result of NPZ file
 
-result = np.load('D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Tree_like_nn_cla_cv_result(Glass-Identification).npz')
+result = np.load('C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Glass-Identification\\Tree_like_nn_cv_result(Glass-Identification).npz')
 
 result.files
 
@@ -281,18 +288,18 @@ result['loss_metric']
 
 from keras.utils import plot_model
 
-plot_model(model, to_file = 'TLNN_glass identification_model0.png')
-plot_model(model, show_shapes = True, to_file = 'TLNN_glass identification_model1.png')
+plot_model(model, to_file = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Figures\\Tree-like\\Classification\\Glass-Identification\\TLNN_glass identification_model0.png')
+plot_model(model, show_shapes = True, to_file = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Figures\\Tree-like\\Classification\\Glass-Identification\\TLNN_glass identification_model1.png')
 
 #----------------------------------------
 # Save the model to H5 file
 
-file_path_hdf5 = 'D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Tree_like_nn_cla_cv(Glass-Identification).h5'
+file_path_hdf5 = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Glass-Identification\\Tree_like_nn_cv(Glass-Identification).h5'
 model.save(file_path_hdf5)
 
 # Load model of H5 file
 
-file_path_hdf5 = 'D:\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Tree_like_nn_cla_cv(Glass-Identification).h5'
+file_path_hdf5 = 'C:\\Users\\jghsieh\\Desktop\\Li-Chun-Ying\\Keras-Objects\\tree-like-nn\\Classification\\Glass-Identification\\Tree_like_nn_cv(Glass-Identification).h5'
 loaded_model = load_model(file_path_hdf5)
 
 #----------------------------------------
@@ -303,16 +310,14 @@ history.history.keys()
 #----------------------------------------
 # Summarize history for loss.
 
-plt.ion()
-
 plt.figure('TLNN Glass-Identification loss', figsize = (4.8, 4.0))
 plt.plot(history.history['loss'], "r-")
 plt.plot(history.history['val_loss'], "b--")
-plt.title('TLNN Glass-Identification Training/validating loss')
+plt.title('TLNN Glass-Identification' + '\n' + 'Training/validating loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['training loss', 'validating loss'], loc = "best", frameon = False)
-#plt.show()
+plt.show()
 
 #----------------------------------------
 # Summarize history for metric.
@@ -320,10 +325,10 @@ plt.legend(['training loss', 'validating loss'], loc = "best", frameon = False)
 plt.figure('TLNN Glass-Identification metric', figsize = (4.8, 4.0))
 plt.plot(history.history['acc'], "r-")
 plt.plot(history.history['val_acc'], "b--")
-plt.title('TLNN Glass-Identification Training/validating metric')
-plt.ylabel('mae')
+plt.title('TLNN Glass-Identification' + '\n' + 'Training/validating metric')
+plt.ylabel('acc')
 plt.xlabel('epoch')
 plt.legend(['training metric', 'validating metric'], loc = "best", frameon = False)
-#plt.show()
+plt.show()
 
 ###################
